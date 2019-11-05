286e0017a7e487e84c228fb37161f5bc59ee3154
RampsosPeck <peralta.jorge.uatf@gmail.com>
2019-11-05 15:06:23 -0400

    Terminado el calendario del dolar y una pequeña carga de modal

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,60 @@
 <template>
   <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>open_in_new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
     <v-content>
-      <v-container>
-          <router-view/>
-      </v-container>
+      <HelloWorld/>
+      <v-dialog
+        v-model="loading.estado"
+        hide-overlay
+        persistent
+        width="300"
+      >
+        <v-card
+          :color="loading.color"
+          dark
+        >
+          <v-card-text>
+            {{ loading.titulo }}
+            <v-progress-linear
+              indeterminate
+              color="white"
+              class="mb-0"
+            ></v-progress-linear>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
     </v-content>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
-
+import {mapState} from 'vuex';
 export default {
   name: 'App',
   components: {
-
+    HelloWorld,
   },
   data: () => ({
     //
   }),
+  computed:{
+    ...mapState(['loading'])
+  }
 };
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,142 +1,67 @@
 <template>
   <v-container>
-    <v-layout
-      text-center
-      wrap
-    >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
+    <v-row>
+        <v-flex xs12>
+          <v-card>
+            <v-date-picker
+              v-model="fecha"
+              full-width
+              locale="es-cl"
+              :min="minimo"
+              :max="maximo"
+              @change="getDolar(fecha)">
 
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
-
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-    </v-layout>
+            </v-date-picker>
+          </v-card>
+          <v-card color="error" dark>
+            <v-card-text class="display-1 text-sm-center">
+              {{ valor }}
+            </v-card-text>
+          </v-card>
+        </v-flex>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import axios from 'axios';
+import { mapMutations } from 'vuex';
 export default {
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
+  data(){
+    return{
+      fecha: new Date().toISOString().substr(0, 10),
+      minimo: '1984',
+      maximo: new Date().toISOString().substr(0, 10),
+      valor: null
+    }
+  },
+  methods: {
+    ...mapMutations(['mostrarLoading','ocultarLoading']),
+      async getDolar(dia){
+        let arrayFecha = dia.split(['-'])
+        let ddmmyy = arrayFecha[2]+'-'+arrayFecha[1]+'-'+arrayFecha[0];
+        try {
+          this.mostrarLoading({titulo:'Accediendo a la información', color:'secondary'})
+
+          let datos = await axios.get(`https://mindicador.cl/api/dolar/${ddmmyy}`)
+          //console.log(datos.data.serie[0].valor);
+          if(datos.data.serie.length > 0){
+            this.valor = await datos.data.serie[0].valor
+          }else{
+            this.valor = 'Sin resultados'
+          }
+        } catch (error) {
+            //console.log(error);
+        }
+        finally {
+          this.ocultarLoading()
+        }
+
+
+      }
+    },
+    created(){
+      this.getDolar(this.fecha)
+    }
 };
 </script>
